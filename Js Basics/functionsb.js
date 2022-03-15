4be3affb4284b97cdd2c788d205ccb9faeb0ea91
sajib <zerosajibint@gmail.com>
2022-03-15 10:41:38 +0600

    js function

--- Js Basics/functionsb.js
+++ Js Basics/functionsb.js
@@ -1,3 +1,10 @@
 function square(number) {
     return number * number;
 }
+function map(f, a) {
+    let result = []; // Create a new Array
+    let i; // Declare variable
+    for (i = 0; i != a.length; i++)
+        result[i] = f(a[i]);
+    return result;
+}
