fb22da31efa23dbad15c74b4ca3cd9928beae534
sajib <zerosajibint@gmail.com>
2022-03-15 10:44:27 +0600

    javascriit functions

--- Js Basics/functionsb.js
+++ Js Basics/functionsb.js
@@ -8,3 +8,17 @@ function map(f, a) {
         result[i] = f(a[i]);
     return result;
 }
+
+function map(f, a) {
+    let result = []; // Create a new Array
+    let i; // Declare variable
+    for (i = 0; i != a.length; i++)
+        result[i] = f(a[i]);
+    return result;
+}
+const f = function (x) {
+    return x * x * x;
+}
+let numbers = [0, 1, 2, 5, 10];
+let cube = map(f, numbers);
+console.log(cube);
